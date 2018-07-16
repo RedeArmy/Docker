fb8148e742686433af7af7837e2d69c5106a6091
Eder Yafeth Garcia Quiroa <ey16_619@hotmail.com>
2018-07-16 14:55:41 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 sleep 5
-if curl localhost | grep -q '<b>visitado:</b>'; then
+if curl ec2-52-90-173-86.compute-1.amazonaws.com:8080 | grep -q '<b>visitado:</b>'; then
   echo "Tests passed!"
   exit 0
 else
