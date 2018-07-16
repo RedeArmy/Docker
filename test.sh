94f3f05742fbbe79c700f5e0767e47e6edb35682
Eder Yafeth Garcia Quiroa <ey16_619@hotmail.com>
2018-07-16 14:42:06 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 sleep 5
 if curl ec2-52-90-173-86.compute-1.amazonaws.com:8080 -X POST | grep -q 'success'; then
-  echo "Tests passed!"
-  exit 0
-else
   echo "Tests failed!"
   exit 1
+else
+  echo "Tests passed!"
+  exit 0
 fi
