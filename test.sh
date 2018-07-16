b6729540615ecd8a2e893e311ec6f121bafa13f0
Eder Yafeth Garcia Quiroa <ey16_619@hotmail.com>
2018-07-16 15:09:12 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 sleep 5
-if curl ec2-52-90-173-86.compute-1.amazonaws.com:8080 | grep -q '<b>visitado:</b>'; then
+if curl 52.90.173.86:8080 | grep -q '<b>visitado:</b>'; then
   echo "Tests failed!"
   exit 1
 else
